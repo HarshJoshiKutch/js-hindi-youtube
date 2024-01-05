c9f6fc7d8e986a534d56934cd6927af9bdc75c22
JOSHI HARSH <124500473+HarshJoshiKutch@users.noreply.github.com>
2024-01-05 07:20:08 +0530

    Add files via upload

---
+++ Async.js
@@ -0,0 +1,9 @@
+// js is synchronous --> jema one code execute thase end ena pachi bijo code execute thase
+// single threaded language che 
+// execute one line of code at a time
+
+// Blocking code vs Non Blocking code 
+// bLocking code ==> Block the flow of program -> Read file sync 
+// Non Blocking code ==> Does not block execution
+
+// promises e fetch ni under aavec che 
